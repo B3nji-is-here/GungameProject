06e9407f9c2a989aab58d51379dc95b7bae72738
BuildTools <unconfigured@null.spigotmc.org>
2021-02-18 17:50:05 +0100

    Edit: Worked on KitManager

--- src/main/java/de/benji/kitsystem/KitManager.java
+++ src/main/java/de/benji/kitsystem/KitManager.java
@@ -1,4 +1,42 @@
 package de.benji.kitsystem;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class KitManager {
+
+    private static KitManager instance;
+
+    private File kitFile = new File(Bukkit.getServer().getPluginManager().getPlugin("Gungame").getDataFolder(), "kits.yml");
+    private YamlConfiguration configuration;
+
+    private ArrayList<Kit> kits = new ArrayList<>();
+
+    private KitManager() {
+        instance = this;
+        if (!kitFile.exists()) {
+            try {
+                kitFile.getParentFile().mkdir();
+                kitFile.createNewFile();
+                configuration = YamlConfiguration.loadConfiguration(kitFile);
+
+                configuration.save(kitFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static KitManager getInstance() {
+        if(instance == null) {
+            new KitManager();
+        }
+        return instance;
+    }
+
+
 }
